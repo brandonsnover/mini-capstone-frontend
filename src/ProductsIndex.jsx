890baf98d1511901008e9fa496eb4db001d35b90
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-12-01 11:50:22 -0600

    added tailwind

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -3,16 +3,23 @@ import { Link } from "react-router-dom";
 export function ProductsIndex(props) {
   return (
     <div>
-      <h1>Our Products</h1>
-      {props.products.map((product) => (
-        <div key={product.id}>
-          <h3>{product.name}</h3>
-          {/* <img src={product.images[0].url} /> */}
-          <p>Description: {product.description}</p>
-          <p>Price: {product.price}</p>
-          <Link to={`/products/${product.id}`}>Go to Product Page</Link>
-        </div>
-      ))}
+      <h1 className="text-3xl font-bold underline">Our Products</h1>
+      <div className="grid grid-cols-4 gap-4">
+        {props.products.map((product) => (
+          <div key={product.id}>
+            <h3>{product.name}</h3>
+            {/* <img src={product.images[0].url} /> */}
+            <p>Description: {product.description}</p>
+            <p>Price: {product.price}</p>
+            <Link
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              to={`/products/${product.id}`}
+            >
+              Go to Product Page
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
